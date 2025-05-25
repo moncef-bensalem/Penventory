674dc2e25db66ca0b2fb45cb1f519ebc374c13df
moncef <moncefbensalem222@gmail.com>
2025-05-25 22:43:26 +0100

    missing payment detail

--- app/(front-end)/checkout/page.jsx
+++ app/(front-end)/checkout/page.jsx
@@ -5,9 +5,9 @@ import Steps from '@/components/Checkout/Steps';
 import React from 'react';
 import { CartProvider } from '@/context/cart-context';
 import { CheckoutProvider } from '@/context/checkout-context';
-import { SessionProvider } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 
-export default function page() {
+export default function Page() {
     const steps = [
   { number: 1, title: "Personal Details" },
   { number: 2, title: "Delivery Details" },
@@ -17,20 +17,18 @@ export default function page() {
 
   // Le code de test a été supprimé car il n'est plus nécessaire
   return (
-    <SessionProvider>
-      <CartProvider>
-        <CheckoutProvider>
-          <div className='bg-slate-100 dark:bg-slate-900 min-h-screen'>
-            <div className="max-w-3xl my-6 mx-auto p-6">
-              <Steps steps={steps}/>
-              <div className='w-full p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700'>
-                <CartBanner/>
-                <StepForm/>
-              </div>
+    <CartProvider>
+      <CheckoutProvider>
+        <div className='bg-slate-100 dark:bg-slate-900 min-h-screen'>
+          <div className="max-w-3xl my-6 mx-auto p-6">
+            <Steps steps={steps}/>
+            <div className='w-full p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700'>
+              <CartBanner/>
+              <StepForm/>
             </div>
           </div>
-        </CheckoutProvider>
-      </CartProvider>
-    </SessionProvider>
+        </div>
+      </CheckoutProvider>
+    </CartProvider>
   )
 }
